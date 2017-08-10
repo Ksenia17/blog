5543c7a22cb4b4db1b77ba5eacf4bcd415015c24
Оксана <mari-mari@ukr.net>
2017-08-10 16:21:21 +0300

    show mistake - css

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :articles
   resource :contacts, only: [:new,:create]
   get 'home/index'
-
+  get 'contacts' => 'contacts#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
