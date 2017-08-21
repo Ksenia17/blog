bfed14912ef507750ebdd7f320018f909bd39eed
Оксана <mari-mari@ukr.net>
2017-08-21 12:43:49 +0300

    add controller comment, action

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'contacts' => 'contacts#new'
 
-  resources :articles
+  resources :articles do
+     resources :comments 
+  end  
   resource :contacts, only: [:new,:create]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
